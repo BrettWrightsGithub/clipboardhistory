efb199db74609c38c0e9deb20ee766128a4124b6
BrettWrightHub <your.email@example.com>
2025-05-03 21:14:15 -0600

    Initial Commit

---
+++ ClipboardHistoryTests/ClipboardHistoryTests.swift
@@ -0,0 +1,17 @@
+//
+//  ClipboardHistoryTests.swift
+//  ClipboardHistoryTests
+//
+//  Created by Brett Wright on 5/3/25.
+//
+
+import Testing
+@testable import ClipboardHistory
+
+struct ClipboardHistoryTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
